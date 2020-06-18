423a105910875f02444657aaf48ba961878ed8d3
iPR3DATOR <predatoraaron38@gmail.com>
2020-06-18 14:20:55 +0800

    Updated by Aaron Chan

---
+++ 17_PA11_AaronChan/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager thisManager;
+
+    public int Score;
+
+    public Text TextScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisManager = this;
+
+        TextScore.text = "Score: " + Score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 17_PA11_AaronChan/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnRed;
+    public GameObject SpawnBrown;
+    public GameObject SpawnGreen;
+
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        int random = Random.Range(1, 4);
+
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+
+        if(random == 1)
+        {
+            Instantiate(SpawnRed, transform.position, transform.rotation);
+        }
+        else if (random == 2)
+        {
+            Instantiate(SpawnBrown, transform.position, transform.rotation);
+        }
+        else if (random == 3)
+        {
+            Instantiate(SpawnGreen, transform.position, transform.rotation);
+        }       
+    }
+}
